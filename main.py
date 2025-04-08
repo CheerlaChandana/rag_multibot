f785b583d4b574ae55e1d66551a56d832dcaac31
CheerlaChandana <106853061+CheerlaChandana@users.noreply.github.com>
2025-04-08 12:18:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 import streamlit as st
 from ui import render_ui
 from processing import load_and_process_documents, refine_question
-from utils import (
-    clean_text,
-    store_feedback,
-    generate_wordcloud,
-    export_chat_to_pdf,
-    process_image,
-)
+from utils import clean_text, store_feedback, generate_wordcloud, export_chat_to_pdf
+import os
 
 if __name__ == "__main__":
-    st.set_page_config(page_title="📄 Advanced Multi-Doc Chat with Groq", layout="wide")
-    render_ui(
-        load_and_process_documents,
-        refine_question,
-        clean_text,
-        store_feedback,
-        generate_wordcloud,
-        export_chat_to_pdf,
-        process_image,
-    )
+    st.set_page_config(page_title="📄 Advanced Multi-Doc Chat with Grok", layout="wide")
+    os.environ['GROQ_API_KEY']="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"
+    render_ui(load_and_process_documents, refine_question, clean_text, store_feedback, generate_wordcloud, export_chat_to_pdf)
